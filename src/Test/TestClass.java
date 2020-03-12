ffa509872e357cc9d7fe948c035635cc2002c7d8
alesao18 <apinto.invest@gmail.com>
2020-03-11 21:33:16 -0400

    one more class added

---
+++ src/Test/TestClass.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class TestClass {
+public static void main(String[] args) {
+	
+	System.out.println(" alec");
+	
+	System.out.println("Hi My Name Is Alejandro tu papi");
+}
+}
